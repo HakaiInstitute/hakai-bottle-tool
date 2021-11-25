a313d1050188239abdd75468f6db14f15edd60e9
Jessy Barrette <jessy.barrette@hakai.org>
2021-11-25 09:19:35 -0800

    Upgrade to version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hakai_bottle_tool",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     py_modules=["hakai_bottle_tool"],
     url="hakai.org",
