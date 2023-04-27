189e2f0ed6821a1110752feec37343696a1e2a05
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-04-27 10:51:38 -0400

    add a pytest run for November 2016 at QU39

---
+++ tests/test_bottle_tool.py
@@ -0,0 +1,9 @@
+import unittest
+
+from hakai_bottle_tool import hakai_bottle_tool
+
+
+class TestBottleMatchingTool(unittest.TestCase):
+    def test_qu39_nov2016_matching(self):
+        df = hakai_bottle_tool.get_bottle_data("QU39", "2016-10-11", "2017-01-01")
+        assert not df.empty, "failed to get any data"
