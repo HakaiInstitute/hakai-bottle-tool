a92fd3fe5cbe0d111030b6d76b6967aae5c9a185
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-04-27 10:58:39 -0400

    add test hakai_id presence

--- tests/test_bottle_tool.py
+++ tests/test_bottle_tool.py
@@ -7,3 +7,4 @@ class TestBottleMatchingTool(unittest.TestCase):
     def test_qu39_nov2016_matching(self):
         df = hakai_bottle_tool.get_bottle_data("QU39", "2016-10-11", "2017-01-01")
         assert not df.empty, "failed to get any data"
+        assert not df.query("phytoplankton_hakai_id == 'QPHY501'").empty, "Missing phytoplankton_hakai_id = 'QPHY501'"
